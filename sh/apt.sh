0ec40f28adee6354285cd8e5d98f3370f96a7632
freebebe <gogofreebebe@tutamail.com>
2021-01-31 20:09:59 +1100

    autobakup

--- sh/apt.sh
+++ sh/apt.sh
@@ -1,20 +1,3 @@
-sudo apt install zip vim neovim curl wget aria2 tree tmux python3 python3-dev python3-pip python3-gpg python3-setuptools ruby-full ruby-sass ncdu fonts-font-awesome preload ranger fzf rclone shellcheck qrencode peek fish proxychains4 dolphin firejail lnav pandoc httpie zplug tree zsh kitty golang nginx -y
+sudo apt install -y zip vim neovim curl wget aria2 tree tmux python3 python3-dev python3-pip python3-gpg python3-setuptools ruby-full ruby-sass ncdu fonts-font-awesome preload ranger fzf rclone shellcheck qrencode peek proxychains4 firejail lnav pandoc httpie zplug tree zsh kitty golang nginx 
 
-# Docker
-sudo apt-get update
-sudo apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-
+source ./docker.sh | source ./rust.sh | source ./npm.sh
---
+++ sh/docker.sh
@@ -0,0 +1,17 @@
+# docker
+sudo apt-get update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fssl https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0ebfcd88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
